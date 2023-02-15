4ee6e2b5f607dce49e01765f18d6caf03b44f028
Nikita <NeXY.Programming@gmail.com>
2023-02-16 00:37:28 +0300

    Front-end development, etc. minor changes
    Design development, small changes in the structure and other project files.

--- GreenhouseManagement/settings.py
+++ GreenhouseManagement/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
